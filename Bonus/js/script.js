88937e8bd54839be80f7a1e71f86c23baa20af64
AlexTaglia <alex.tagliabue@gmail.com>
2021-06-28 15:02:09 +0200

    Bonus

---
+++ Bonus/js/script.js
@@ -0,0 +1,51 @@
+Vue.config.devtools = true;
+
+new Vue(
+    {
+        el: '#app',
+        data: {
+            contentImg: [
+                './img/img1.jpg',
+                './img/img2.jpg',
+                './img/img3.jpg',
+                './img/img4.jpg',
+                './img/img5.jpg',
+                './img/img6.jpg',
+            ],
+            imgIndex: 0,
+        },
+        created() {
+            setInterval(() => {
+                this.next();
+            }, 3000); 
+        },
+        methods: {
+            next: function () {
+                if (this.imgIndex === (this.contentImg.length - 1)) {
+                    this.imgIndex = 0;
+                    this.fadeClass = 'fade'
+                } else {
+                    this.imgIndex++
+                }
+            },
+            previous: function () {
+                if (this.imgIndex === 0) {
+                    this.imgIndex = this.contentImg.length - 1
+                } else {
+                    this.imgIndex--
+                };
+            },
+            currentDot: function (index) {
+                if (this.imgIndex === index) {
+                    return 'current-dot'
+                } else {
+                    return '';
+                }
+            },
+            goTo: function (index) {
+                this.imgIndex = index;
+            }
+        }
+    }
+
+)
